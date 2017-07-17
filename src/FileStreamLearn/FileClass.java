44d93f5197f3f0062f9a2176f5421f63105ab2d4
lhd24 <haidong@DESKTOP-BJ2GFH0>
2017-07-17 17:41:59 +0800

    文件操作

---
+++ src/FileStreamLearn/FileClass.java
@@ -0,0 +1,61 @@
+/**
+ * 功能: File类的基本用法
+ */
+
+package FileStreamLearn;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileClass  {
+
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void test1(){
+		//创建一个文件对象
+		File f = new File("d:\\lhd.txt");
+		
+		//得到文件的路径
+		System.out.println("文件路径:" + f.getAbsolutePath());
+		//得到文件的大小，字节数
+		System.out.println("文件大小:" + f.length());
+		System.out.println("可读" + f.canRead());
+	}
+	
+	public void test2() throws IOException {
+		//创建文件和创建文件夹
+		File f = new File("d:\\lhd.txt");
+		if (!f.exists()) {
+			//可以创建
+			f.createNewFile();
+		}
+		else{
+			System.out.println("有文件，不能创建");
+		}
+	}
+	
+	public void test3(){
+		File f = new File("d:\\ff");
+		if (f.isDirectory()) {
+			System.out.println("文件夹已经存在");
+		}
+		else{
+			//创建
+			f.mkdir();
+		}
+	}
+	
+	//列出一个文件夹下面的所有文件
+	public void test4(){
+		File f = new File("d:\\ff");
+		if (f.isDirectory()) {
+			File lists[] = f.listFiles();
+			for (int i = 0; i < lists.length; i++) {
+				System.out.println("文件名:" + lists[i].getName());
+			}
+		}
+	}
+}
